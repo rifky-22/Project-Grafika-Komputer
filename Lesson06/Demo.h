93e8d6c030dd0a3ceba8aeeb5315e1b4716364ad
rifky-22 <81907672+rifky-22@users.noreply.github.com>
2022-10-28 23:00:52 +0700

    Add files via upload

---
+++ Lesson06/Demo.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "RenderEngine.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/vector_angle.hpp>
+#include <SOIL/SOIL.h>
+
+class Demo :
+	public RenderEngine
+{
+public:
+	Demo();
+	~Demo();
+private:
+	GLuint shaderProgram, VBO, VAO, EBO, texture, VBO2, VAO2, EBO2, texture2;
+	float viewCamX, viewCamY, viewCamZ, upCamX, upCamY, upCamZ, posCamX, posCamY, posCamZ, CAMERA_SPEED, fovy;
+	virtual void Init();
+	virtual void DeInit();
+	virtual void Update(double deltaTime);
+	virtual void Render();
+	virtual void ProcessInput(GLFWwindow *window);
+	void BuildColoredCube();
+	void BuildColoredPlane();
+	void DrawColoredCube(float s1, float s2, float s3, float t1, float t2, float t3, float c1, float c2, float c3);
+	void DrawColoredPlane();
+	void MoveCamera(float speed);
+	void StrafeCamera(float speed);
+	void RotateCamera(float speed);
+	void InitCamera();
+};
+
